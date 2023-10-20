edaa0b8ef642d4b4888ff6caf2e16d357ede2f9f
seymen-elmas <seymennadirelmas@gmail.com>
2023-10-20 14:22:10 +0300

    maps loaded

--- IOS/Whereis/Whereis/ContentView.swift
+++ IOS/Whereis/Whereis/ContentView.swift
@@ -6,21 +6,32 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+   @StateObject private var mapAPI = MapAPI()
+   @State private var text = ""
+   
+   var body: some View {
+      VStack {
+         TextField("Enter an address", text: $text)
+            .textFieldStyle(.roundedBorder)
+            .padding(.horizontal)
+         
+         Button("Find address") {
+            mapAPI.getLocation(address: text, delta: 0.5)
+         }
+         
+         Map(coordinateRegion: $mapAPI.region, annotationItems: mapAPI.locations) { location in
+            MapMarker(coordinate: location.coordinate, tint: .blue)
+         }
+         .ignoresSafeArea()
+      }
+   }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+   static var previews: some View {
+      ContentView()
+   }
 }
